b8ed8828575f49909b62d874e0ab20c209c593bf
Shravan Kapavarapu <4694738+skapavarapu@users.noreply.github.com>
2020-06-02 18:04:46 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
